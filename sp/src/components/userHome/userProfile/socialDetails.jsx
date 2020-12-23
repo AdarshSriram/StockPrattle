2cf62ceb65b35a34f5c49100428a7f46d9697b3a
Adarsh Sriram <as2566@cornell.edu>
2020-12-23 21:55:25 +0530

    smallchange

--- sp/src/components/userHome/userProfile/socialDetails.jsx
+++ sp/src/components/userHome/userProfile/socialDetails.jsx
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { followUser } from "./../../../firebase_functions"
 
-export default class SocialDetails extends Component{
-    constructor(props){
+export default class SocialDetails extends Component {
+    constructor(props) {
         super(props);
-        this.state = {following: props.following}
+        this.state = { following: props.following }
         this.followMouseIn = this.followMouseIn.bind(this)
         this.followMouseOut = this.followMouseOut.bind(this)
         this.followUnfollow = this.followUnfollow.bind(this)
     }
 
-    componentDidUpdate(prevProps){
-        if (prevProps.user != this.props.user){
-            this.setState({following: this.props.following})
+    componentDidUpdate(prevProps) {
+        if (prevProps.following != this.props.following) {
+            this.setState({ following: this.props.following })
         }
     }
 
-    mouseIn(event){
+    mouseIn(event) {
         event.target.style.textDecoration = "underline"
     }
 
-    mouseOut(event){
+    mouseOut(event) {
         event.target.style.textDecoration = "none"
     }
 
     followMouseIn(but) {
-        if (this.state.following){
+        if (this.state.following) {
             but.target.style.background = "#00B140";
             but.target.style.color = "#FFFFFF";
         } else {
@@ -34,7 +35,7 @@ export default class SocialDetails extends Component{
     }
 
     followMouseOut(but) {
-        if (this.state.following){
+        if (this.state.following) {
             but.target.style.background = "#FFFFFF";
             but.target.style.color = "#00B140";
         } else {
@@ -42,14 +43,16 @@ export default class SocialDetails extends Component{
         }
     }
 
-    followUnfollow(){
+    followUnfollow() {
         const email = this.props.user.email
-        this.setState({following: !this.state.following})
+        const uname = this.props.user.username
+        followUser(email, uname)
+        this.setState({ following: !this.state.following })
     }
 
-    render(){
-        return(
-        <div style={{
+    render() {
+        return (
+            <div style={{
                 height: "100%",
                 width: "100%",
                 gap: (this.props.name == null) ? "20px" : "10px",
@@ -63,33 +66,34 @@ export default class SocialDetails extends Component{
                 background: "none",
                 // overflow: "scroll"
             }}>
-            <button style={socialStyle.button} onMouseOver={this.mouseIn} onMouseLeave={this.mouseOut}
-            >{(this.props.name == null) ? "Stocks You Follow" : "Stocks "+this.props.name.split(" ")[0]+" Follows"}</button>
-            <button style={socialStyle.button} onMouseOver={this.mouseIn} onMouseLeave={this.mouseOut}
-            >{(this.props.name == null) ? "People You Follow" : "People "+this.props.name.split(" ")[0]+" Follows"}</button>
-            <button style={socialStyle.button} onMouseOver={this.mouseIn} onMouseLeave={this.mouseOut}
-            >{(this.props.name == null) ? "People Who Follow You" : "People Who Follow "+this.props.name.split(" ")[0]}</button>
-            {(this.props.name == null) ? null : <button style={{
-                background: (this.state.following) ? "#FFFFFF" : "#00B140",
-                width: "80px",
-                height: "35px",
-                borderRadius: "15px",
-                border: (this.state.following) ? "1px dashed #00B140" :"0px dashed #FFFFFF",
-                color: (this.state.following) ? "#00B140" : "#FFFFFF",
-                fontFamily: "Dosis",
-                fontStyle: "normal",
-                fontWeight: "bold",
-                fontSize: "18px",
-                outline: "none",
-                cursor: "pointer"
-            }} onClick={this.followUnfollow}
-            onMouseOver={this.followMouseIn} onMouseLeave={this.followMouseOut}>{(this.state.following) ? "Unfollow" : "Follow"}</button>}
-        </div>
+                <button style={socialStyle.button} onMouseOver={this.mouseIn} onMouseLeave={this.mouseOut}
+                >{(this.props.name == null) ? "Stocks You Follow" : "Stocks " + this.props.name.split(" ")[0] + " Follows"}</button>
+                <button style={socialStyle.button} onMouseOver={this.mouseIn} onMouseLeave={this.mouseOut}
+                >{(this.props.name == null) ? "People You Follow" : "People " + this.props.name.split(" ")[0] + " Follows"}</button>
+                <button style={socialStyle.button} onMouseOver={this.mouseIn} onMouseLeave={this.mouseOut}
+                >{(this.props.name == null) ? "People Who Follow You" : "People Who Follow " + this.props.name.split(" ")[0]}</button>
+                {(this.props.name == null) ? null : <button style={{
+                    background: (this.state.following) ? "#FFFFFF" : "#00B140",
+                    width: "80px",
+                    height: "35px",
+                    borderRadius: "15px",
+                    border: (this.state.following) ? "1px dashed #00B140" : "0px dashed #FFFFFF",
+                    color: (this.state.following) ? "#00B140" : "#FFFFFF",
+                    fontFamily: "Dosis",
+                    fontStyle: "normal",
+                    fontWeight: "bold",
+                    fontSize: "18px",
+                    outline: "none",
+                    cursor: "pointer"
+                }} onClick={this.followUnfollow}
+                    onMouseOver={this.followMouseIn} onMouseLeave={this.followMouseOut}>{(this.state.following) ? "Unfollow" : "Follow"}</button>}
+            </div>
         )
     }
 }
 
-const socialStyle = {button: {
+const socialStyle = {
+    button: {
         background: "none",
         height: "32px",
         borderRadius: "30px",
