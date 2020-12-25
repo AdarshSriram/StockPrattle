bfa7138ed0718c7e38ce201f44e8f378224a9f4e
Aayush Chowdhry <ac2447@cornell.edu>
2020-12-25 12:06:08 +0530

    Follow button on stock page.

--- sp/src/components/userHome/explore/stockPage.jsx
+++ sp/src/components/userHome/explore/stockPage.jsx
@@ -6,6 +6,32 @@ import UserFeed from '../userFeed.jsx';
 export default class StockPage extends Component{
     constructor(props){
         super(props);
+        this.state = {following: props.following}
+        this.followMouseIn = this.followMouseIn.bind(this)
+        this.followMouseOut = this.followMouseOut.bind(this)
+        this.followUnfollow = this.followUnfollow.bind(this)
+    }
+
+    followMouseIn(but) {
+        if (this.state.following) {
+            but.target.style.background = "#00B140";
+            but.target.style.color = "#FFFFFF";
+        } else {
+            but.target.style.background = "#009435";
+        }
+    }
+
+    followMouseOut(but) {
+        if (this.state.following) {
+            but.target.style.background = "#FFFFFF";
+            but.target.style.color = "#00B140";
+        } else {
+            but.target.style.background = "#00B140";
+        }
+    }
+
+    followUnfollow() {
+        this.setState({ following: !this.state.following })
     }
 
     render(){
@@ -20,6 +46,21 @@ export default class StockPage extends Component{
                     <p style = {stockStyle.text}><b>High:</b> $$$</p>
                     <p style = {stockStyle.text}><b>Volume:</b> $$$</p>
                     <p style = {stockStyle.text}><b>SP Predictor:</b> $$$</p>
+                    <button style={{
+                        background: (this.state.following) ? "#FFFFFF" : "#00B140",
+                        width: "80px",
+                        height: "35px",
+                        borderRadius: "15px",
+                        border: (this.state.following) ? "1px dashed #00B140" : "0px dashed #FFFFFF",
+                        color: (this.state.following) ? "#00B140" : "#FFFFFF",
+                        fontFamily: "Dosis",
+                        fontStyle: "normal",
+                        fontWeight: "bold",
+                        fontSize: "18px",
+                        outline: "none",
+                        cursor: "pointer"
+                    }} onClick={this.followUnfollow}
+                        onMouseOver={this.followMouseIn} onMouseLeave={this.followMouseOut}>{(this.state.following) ? "Unfollow" : "Follow"}</button>
                 </div>
             </div>
             <UserFeed user={null}/>
@@ -72,7 +113,7 @@ const stockStyle= { centerDiv: {
         padding: "15px",
         flexDirection: "column",
         justifyContent: "center",
-        alignItems: "flex-start",
+        alignItems: "center",
         gap: "15px",
         // border: "thick solid black",
         boxSizing: "border-box",
