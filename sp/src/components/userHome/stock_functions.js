d86c35f0809c570fa9f6f342faaca9649c838f45
Adarsh Sriram <as2566@cornell.edu>
2021-02-06 15:05:19 -0500

    Get history

--- sp/src/components/userHome/stock_functions.js
+++ sp/src/components/userHome/stock_functions.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-const test = "http://nimblerest.lisuns.com:4531/GetExchangeSnapshot/?accessKey=eac386of-b529-4119-8252-f0a435185986&exchange=NSE&periodicity=Minute&period=15&from=1610100000&to=1610110198"
+const test = "http://nimblerest.lisuns.com:4531/GetExchangeSnapshot/?accessKey=eac3860f-b529-4119-8252-f0a435185986&exchange=NSE&periodicity=Minute&period=15&from=1610100000&to=1610110198"
 
 
-const history = "http://nimblerest.lisuns.com:4531/GetHistory/?accessKey=eac386of-b529-4119-8252-f0a435185986&exchange=NSE&periodicity=Minute&period=15&from="
+const history1 = "http://nimblerest.lisuns.com:4531/GetHistory/?accessKey=eac3860f-b529-4119-8252-f0a435185986&exchange=NSE&instrumentIdentifier="
+
+const history2 = "&periodicity=Minute&period=15&FROM="
 
 export const getSnapshot = () => {
   console.log("sending request")
@@ -13,29 +15,28 @@ export const getSnapshot = () => {
   }).catch(err => { console.log(err); return [] })
 }
 
-export const getHistory  = () => {
-    console.log("sending request")
-    const to = Math.floor(Date.now()/1000)
-    const from = to - 432000
-    var toDate = new Date()
+export const getHistory = (stock) => {
+  console.log("sending request")
+  const to = Math.floor(Date.now() / 1000)
+  const from = to - 432000
+  var toDate = new Date()
 
-    var fromDate = new Date(from*1000)
+  var fromDate = new Date(from * 1000)
 
-    if (fromDate.getDay() == 6 || toDate.getDay() == 6){
-        from -= 86400
-    }
-    else if (fromDate.getDay() == 0 || toDate.getDay() == 0){
-        from -= 86400*2
-    }
-    const time  = from % 86400
+  if (fromDate.getDay() == 6 || toDate.getDay() == 6) {
+    from -= 86400
+  }
+  else if (fromDate.getDay() == 0 || toDate.getDay() == 0) {
+    from -= 86400 * 2
+  }
+  const time = from % 86400
 
-    time < 16200 ? from += 16250 - time
+  //from += time < 16200 ? 16250 - time : 0
 
-    return axios.get(historyData+from + "&to=" + to).then((res) => {
-      console.log("response recieved")
-      return res.data.EXCHANGESNAPSHOTITEMS[0].SNAPSHOTITEMS.map(obj => obj.INSTRUMENTIDENTIFIER)
-    }).catch(err => { console.log(err); return [] })
-    }
+  return axios.get(history1 + stock + history2 + from + "&to=" + to).then((res) => {
+    console.log("response recieved")
+    return res.data["OHLC"]
+  }).catch(err => { console.log(err); return [] })
 }
 
 export const getInstruments = (arr) => {
