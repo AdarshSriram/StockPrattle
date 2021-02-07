03bfb54484f1510a6d159ef15a177f8c103a3475
Adarsh Sriram <as2566@cornell.edu>
2021-02-07 16:22:10 -0500

    snas

--- sp/src/components/userHome/stock_functions.js
+++ sp/src/components/userHome/stock_functions.js
@@ -2,22 +2,25 @@ import axios from 'axios'
 
 const key = process.env.REACT_APP_DATA_KEY
 
-const test = "http://nimblerest.lisuns.com:4531/GetExchangeSnapshot/?accessKey" + key + "&exchange=NSE&periodicity=Minute&period=15"
+console.log(key)
+
+const test = "http://nimblerest.lisuns.com:4531/GetExchangeSnapshot/?accessKey=" + key + "&exchange=NSE&periodicity=Minute&period=15"
 
 const history1 = "http://nimblerest.lisuns.com:4531/GetHistory/?accessKey=" + key + "&exchange=NSE&instrumentIdentifier="
 
 const history2 = "&periodicity=Minute&period=15&FROM="
 
 export const getSnapshot = () => {
-  /*var to = Math.floor(Date.now() / 1000)
-  if (to % 86400 >= 36000) {
-    to -= to % 86400 - 36050
-  }
-  var from = to -= 900*/
   console.log("sending request")
   return axios.get(test).then((res) => {
     console.log("response recieved")
-    return res.data.EXCHANGESNAPSHOTITEMS[0].SNAPSHOTITEMS
+    try {
+      console.log(res.data.EXCHANGESNAPSHOTITEMS)
+      return res.data.EXCHANGESNAPSHOTITEMS[0].SNAPSHOTITEMS
+    }
+    catch {
+      getSnapshot()
+    }
   }).catch(err => { console.log(err); return [] })
 }
 
@@ -60,16 +63,6 @@ export const getInstruments = (arr) => {
 const nifty50 = ['ADANIPORTS', 'ASIANPAINT', 'AXISBANK', 'BAJAJ-AUTO', 'BAJFINANCE', 'BAJAJFINSV', 'BPCL', 'BHARTIARTL', 'BRITANNIA', 'CIPLA', 'COALINDIA', 'DIVISLAB', 'DRREDDY', 'EICHERMOT', 'GAIL', 'GRASIM', 'HCLTECH', 'HDFCBANK', 'HDFCLIFE', 'HEROMOTOCO', 'HINDALCO', 'HINDUNILVR', 'HDFC', 'ICICIBANK', 'ITC', 'IOC', 'INDUSINDBK', 'INFY', 'JSWSTEEL', 'KOTAKBANK', 'LT', 'M&M', 'MARUTI', 'NTPC', 'NESTLEIND', 'ONGC', 'POWERGRID', 'RELIANCE', 'SBILIFE', 'SHREECEM', 'SBIN', 'SUNPHARMA', 'TCS', 'TATAMOTORS', 'TATASTEEL', 'TECHM', 'TITAN', 'UPL', 'ULTRACEMCO', 'WIPRO']
 
 const niftySnap = "http://nimblerest.lisuns.com:4531/GetSnapshot/?accessKey=" + key + "&exchange=NSE&periodicity=Minute&period=1&&instrumentIdentifiers="
-
-export const getNifty = () => {
-  console.log("sending request")
-  return axios.get(niftySnap + nifty50.slice(0, 24).join('+')).then((res1) => {
-    axios.get(niftySnap + nifty50.slice(25, 49).join('+')).then(res2 => {
-      console.log("response recieved")
-      return [res1.data, res2.data]
-    })
-  }).catch(err => { console.log(err); return [] })
-}
 
 export const getStocksData = (symbols) => {
   console.log("sending request")
