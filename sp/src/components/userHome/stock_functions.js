2173b033e1aa5a1813d8441a1a9051874d9272a7
Adarsh Sriram <as2566@cornell.edu>
2021-02-06 21:09:53 -0500

    Hidden keys

--- sp/src/components/userHome/stock_functions.js
+++ sp/src/components/userHome/stock_functions.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-const test = "http://nimblerest.lisuns.com:4531/GetExchangeSnapshot/?accessKey=eac3860f-b529-4119-8252-f0a435185986&exchange=NSE&periodicity=Minute&period=15&from=1610100000&to=1610110198"
+const key = process.env.REACT_APP_DATA_KEY,
+
+const test = "http://nimblerest.lisuns.com:4531/GetExchangeSnapshot/?accessKey" + key + "&exchange=NSE&periodicity=Minute&period=15&from=1610100000&to=1610110198"
 
 
-const history1 = "http://nimblerest.lisuns.com:4531/GetHistory/?accessKey=eac3860f-b529-4119-8252-f0a435185986&exchange=NSE&instrumentIdentifier="
+const history1 = "http://nimblerest.lisuns.com:4531/GetHistory/?accessKey=" + key + "&exchange=NSE&instrumentIdentifier="
 
 const history2 = "&periodicity=Minute&period=15&FROM="
 
@@ -15,23 +17,26 @@ export const getSnapshot = () => {
   }).catch(err => { console.log(err); return [] })
 }
 
-export const getHistory = (stock) => {
+export const getHistory = (stock, fromTime = -1) => {
   console.log("sending request")
   const to = Math.floor(Date.now() / 1000)
-  const from = to - 432000
-  var toDate = new Date()
+  if (fromTime != -1) {
+    const from = to - 432000
+    var toDate = new Date()
 
-  var fromDate = new Date(from * 1000)
+    var fromDate = new Date(from * 1000)
 
-  if (fromDate.getDay() == 6 || toDate.getDay() == 6) {
-    from -= 86400
+    if (fromDate.getDay() == 6 || toDate.getDay() == 6) {
+      from -= 86400
+    }
+    else if (fromDate.getDay() == 0 || toDate.getDay() == 0) {
+      from -= 86400 * 2
+    }
+    const time = from % 86400
+    //from += time < 16200 ? 16250 - time : 0
   }
-  else if (fromDate.getDay() == 0 || toDate.getDay() == 0) {
-    from -= 86400 * 2
-  }
-  const time = from % 86400
 
-  //from += time < 16200 ? 16250 - time : 0
+  else { var from = fromTime }
 
   return axios.get(history1 + stock + history2 + from + "&to=" + to).then((res) => {
     console.log("response recieved")
@@ -49,7 +54,7 @@ export const getInstruments = (arr) => {
 
 const nifty50 = ['ADANIPORTS', 'ASIANPAINT', 'AXISBANK', 'BAJAJ-AUTO', 'BAJFINANCE', 'BAJAJFINSV', 'BPCL', 'BHARTIARTL', 'BRITANNIA', 'CIPLA', 'COALINDIA', 'DIVISLAB', 'DRREDDY', 'EICHERMOT', 'GAIL', 'GRASIM', 'HCLTECH', 'HDFCBANK', 'HDFCLIFE', 'HEROMOTOCO', 'HINDALCO', 'HINDUNILVR', 'HDFC', 'ICICIBANK', 'ITC', 'IOC', 'INDUSINDBK', 'INFY', 'JSWSTEEL', 'KOTAKBANK', 'LT', 'M&M', 'MARUTI', 'NTPC', 'NESTLEIND', 'ONGC', 'POWERGRID', 'RELIANCE', 'SBILIFE', 'SHREECEM', 'SBIN', 'SUNPHARMA', 'TCS', 'TATAMOTORS', 'TATASTEEL', 'TECHM', 'TITAN', 'UPL', 'ULTRACEMCO', 'WIPRO']
 
-const niftySnap = "http://nimblerest.lisuns.com:4531/GetSnapshot/?accessKey=f841e838-18c1-4a48-bdb3-7a0273dc78af&exchange=NSE&periodicity=Minute&period=1&&instrumentIdentifiers="
+const niftySnap = "http://nimblerest.lisuns.com:4531/GetSnapshot/?accessKey=" + key + "&exchange=NSE&periodicity=Minute&period=1&&instrumentIdentifiers="
 
 export const getNifty = () => {
   console.log("sending request")
--- sp/src/utils/config.js
+++ sp/src/utils/config.js
@@ -2,13 +2,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyD7_c5e4zdaJ5fgaSPHu2ypqv0UH5m49os",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "stockprattle.firebaseapp.com",
   databaseURL: "https://stockprattle.firebaseio.com",
   projectId: "stockprattle",
   storageBucket: "stockprattle.appspot.com",
   messagingSenderId: "867437344780",
-  appId: "1:867437344780:web:4d94750a3fb5c19238d629",
+  appId: process.env.REACT_APP_APP_ID,
   measurementId: "G-75LCH0YZ91"
 };
 const fire = firebase.initializeApp(config);
