081792ca3a5bd49f57cde9b9471da7b08d33d277
Adarsh Sriram <60590530+AdarshSriram@users.noreply.github.com>
2021-08-16 15:04:15 -0400

    Update config.js

--- sp/src/utils/config.js
+++ sp/src/utils/config.js
@@ -2,14 +2,6 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "stockprattle.firebaseapp.com",
-  databaseURL: "https://stockprattle.firebaseio.com",
-  projectId: "stockprattle",
-  storageBucket: "stockprattle.appspot.com",
-  messagingSenderId: "867437344780",
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: "G-75LCH0YZ91"
 };
 const fire = firebase.initializeApp(config);
 export default fire;
